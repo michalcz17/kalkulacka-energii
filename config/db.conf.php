211f494183b788e5ff9fd0added025bc816479cc
Michal Zachař <michal.zachy@gmail.com>
2023-05-27 18:23:51 +0200

    Aktualizace databáze
    Databázové připojení se nyní navazuje až po načtení konfiguračních souborů

--- config/db.conf.php
+++ config/db.conf.php
@@ -5,6 +5,4 @@ $database["database"] = "";
 $database["username"] = "";
 $database["password"] = "";
 
-$mysqli = new mysqli($database["hostname"], $database["username"], $database["password"], $database["database"]);
-
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 require("./config/loader.php");
 require("./class/loader.php");
 
+/* Vytvoření databázového připojení */
+$mysqli = new mysqli($database["hostname"], $database["username"], $database["password"], $database["database"]);
+
 /* handler pro formuláře pokud je nějaký odeslán */
 if(!empty($_POST)){
     require("./server/formHandler.php");
